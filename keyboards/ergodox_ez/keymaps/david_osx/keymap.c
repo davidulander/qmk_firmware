e19368f4121b4f26ef9a0cfb0c6cc0f9153536b8
David Ulander <david.ulander@gmail.com>
2021-04-14 10:59:28 +0200

    move control

--- keyboards/ergodox_ez/keymaps/david_osx/keymap.c
+++ keyboards/ergodox_ez/keymaps/david_osx/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| ScDn |           | Alt  |------+------+------+------+------+--------|
  * | Shift  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   -  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      | Ctrl |  Alt |  CMD |                                       | Shift| Ctrl |      |      |       |
+ *   |      |      | Ctrl |  Alt |  CMD |                                       | Ctrl |      |      |      |       |
  *   `----------------------------------'                                       `-----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -49,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_NO,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,             KC_MINS,
              KC_ESC,       KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,             KC_LBRACKET,
                            KC_H,           LT(SYMB,KC_J),  KC_K,           KC_L,           KC_SCOLON,        KC_QUOTE,
-             KC_RALT,   KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,          KC_NO,
-                                           KC_RSFT,        KC_RCTL,        KC_NO,          KC_NO,            KC_NO,
+             KC_RALT,      KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,          KC_NO,
+                                           KC_RCTL,        KC_NO,          KC_NO,          KC_NO,            KC_NO,
              KC_NO,   KC_NO,
              KC_RSFT,
              KC_RGUI, KC_ENT, KC_SPC
