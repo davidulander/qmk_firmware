e4aca94a074b755406fa5810e2f53a426d3a009e
David Ulander <david.ulander@gmail.com>
2023-12-15 09:01:11 +0100

    move L5

--- keyboards/ergodox_ez/keymaps/david_osx/keymap.c
+++ keyboards/ergodox_ez/keymaps/david_osx/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   ?+   |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  | L5   |           |      |   6  |   7  |   8  |   9  |   0  |   ?+   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  | E/L3 |   R  |   T  | ScUp |           |      |   Y  |   U  |   I  |   O  |   P  |   Å    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| ScDn |           | ESC  |------+------+------+------+------+--------|
  * | Shift  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   -  |   @    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L5  | HYPER| Ctrl |  Alt |  CMD |                                       | Ctrl | HYPER|      |      |       |
+ *   |      | HYPER| Ctrl |  Alt |  CMD |                                       | Ctrl | HYPER|      |      |       |
  *   `----------------------------------'                                       `-----------------------------------'
  *                                        ,-------------.       ,--------------.
  *                                        | ScLf | ScRt |       | Frwrd|       |
@@ -44,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,           KC_1,             KC_2,             KC_3,                 KC_4,               KC_5,               KC_NO,
+        KC_ESC,           KC_1,             KC_2,             KC_3,                 KC_4,               KC_5,               TG(MOUSE_LEFT),
         KC_TAB,           KC_Q,             KC_W,             LT(MOUS,KC_E),        KC_R,               KC_T,               KC_WH_U,
         KC_NO,            KC_A,             LT(NUMB,KC_S),    LT(ARRW,KC_D),        LT(SYMB,KC_F),      KC_G,
         KC_LSFT,          KC_Z,             KC_X,             KC_C,                 KC_V,               KC_B,               KC_WH_D,
-        TG(MOUSE_LEFT),   ALL_T(KC_NO),     KC_LCTL,          KC_LALT,              KC_LGUI,
+        KC_NO,            ALL_T(KC_NO),     KC_LCTL,          KC_LALT,              KC_LGUI,
                                                                                       KC_WH_L,              KC_WH_R,
                                                                                       SGUI(KC_F12),
                                                                                       KC_BSPC,  KC_DEL,     SH,
@@ -231,7 +231,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 4: Number Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      | L1   |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |  TAB   |      |      |      |      |      |      |           | ScUp |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -239,7 +239,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           | ScDn |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1  |      | CTRL | ALT  | CMD  |                                       |      |      |      |      |      |
+ *   |      |      | CTRL | ALT  | CMD  |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -253,9 +253,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MOUSE_LEFT] = LAYOUT_ergodox(
        // left hand
        KC_TRNS,    KC_TRNS,     KC_TRNS,      	KC_TRNS,   	   KC_TRNS,    KC_TRNS,   	KC_TRNS,
-       KC_TAB,     KC_TRNS,     KC_TRNS,	    KC_TRNS, 	   KC_TRNS,    KC_TRNS,     KC_TRNS,
+       KC_TAB,     KC_TRNS,     KC_TRNS,	      KC_TRNS, 	     KC_TRNS,    KC_TRNS,     KC_TRNS,
        KC_TRNS,    KC_TRNS,     KC_TRNS,        KC_TRNS,       KC_TRNS,    KC_TRNS,
-       KC_TRNS,    KC_TRNS,     KC_TRNS,        KC_TRNS, 	   KC_TRNS,    KC_TRNS,     KC_TRNS,
+       KC_TRNS,    KC_TRNS,     KC_TRNS,        KC_TRNS,
+       KC_TRNS,    KC_TRNS,     KC_TRNS,
        KC_TRNS,    KC_TRNS,     KC_LCTL,        KC_LALT,       KC_LGUI,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
